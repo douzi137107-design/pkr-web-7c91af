c8b4df483dfb9165466610aeee0470c4e49327a2
douzi137107-design <douzi137107@gmail.com>
2026-07-21 08:18:06 +0800

    Update relationship energy lab

--- assets/section-7xPOhA3s.js
+++
@@ -1,1 +0,0 @@
-import{g as t,k as o,r,l as s}from"./app-BFwgtnC5.js";function u(){!t.current&&o();const[e]=r.useState(s.current);return e}export{u};
---
+++ assets/section-CZSWLihm.js
@@ -0,0 +1,1 @@
+const a="relationship-lab:azhe-chapter-1",t="relationship-lab:xiaoman-chapter-1";export{a as A,t as X};
